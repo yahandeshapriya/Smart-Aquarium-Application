72739960126b430586c800aa9e947351f7fd533c
yahandeshapriya <yahandeshapriya@gmail.com>
2022-05-21 06:34:18 +0530

    UI were updated

---
+++ app/src/main/java/com/example/smartaquarium/Adduser.java
@@ -0,0 +1,14 @@
+package com.example.smartaquarium;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Adduser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adduser);
+    }
+}
